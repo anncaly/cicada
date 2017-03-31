88cd2c5209747ec0ae8f707383cb996c5f7f5b0b
anncaly <anncaly@gmail.com>
2017-03-31 12:22:55 +0300

    start project

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const gulp         = require('gulp');
+const sass         = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps   = require('gulp-sourcemaps');
+const jade         = require('gulp-jade');
+const pug          = require('gulp-pug');
+const notify       = require('gulp-notify');
+
+gulp.task('jade', function(done) {
+    return gulp.src('./*.jade')
+        .pipe(jade())
+        .pipe(gulp.dest('build'));
+done();
+});
+
+gulp.task('sass', function(done) {
+    return gulp.src('scss/*.scss')
+        .pipe(sass())
+        // .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(gulp.dest('css'));
+done();
+});
+
+gulp.task('watch', function(done) {
+  gulp.watch('scss/*.scss' ['sass']);
+  gulp.watch('./*.jade' ['jade']);
+done();    
+});
